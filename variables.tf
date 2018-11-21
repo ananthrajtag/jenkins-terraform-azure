2fe5d6e3fdcc1f4ef23e8ea6ba1fcdc7681d89eb
gbpeva3 <phil.evans@atradius.com>
2018-11-21 15:54:01 +0000

    Changed user input variables.

--- variables.tf
+++ variables.tf
@@ -2,12 +2,8 @@ variable "env-type" {
   default = "dev"
 }
 
-variable "env-number" {
-  default = "999"
-}
-
 variable "prefix" {
-  default = "dev-env-02"
+  default = "-env-02"
 }
 
 variable "location" {
